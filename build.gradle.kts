b72e3694660bc05b5e4eeddec124a668db73d80e
Moritz Reiter <mail@moritzreiter.de>
2024-06-22 16:17:32 +0200

    add session management

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,19 +18,23 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2023.0.2"
+extra["testcontainersVersion"] = "1.17.3"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	implementation("org.springframework.session:spring-session-data-redis")
 	runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.111.Final:osx-aarch_64")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:junit-jupiter")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+		mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
 	}
 }
 
